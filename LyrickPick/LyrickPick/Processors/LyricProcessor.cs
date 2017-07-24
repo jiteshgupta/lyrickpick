8771a6583e58ec7a0cced610f0538ca6094eb057
jesschen <jessica_j_chen@outlook.com>
2017-07-24 14:34:56 -0700

    added song class and lyric proccessor

---
+++ LyrickPick/LyrickPick/Processors/LyricProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LyrickPick.Processors
+{
+    public class LyricProcessor
+    {
+        //takes in json or string of one song's lyrics
+        //returns as list of each line
+        public static string[] SpliceSong (string SongLyrics)
+        {
+            //array list of every line as a separate item
+            string[] lines = new string[SongLyrics.Length];
+            return lines;
+
+        }
+        //takes in list of each line
+        public static List<Tuple<string, int>> CountLines (string[] SongLyrics)
+        {
+            List<Tuple<string, int>> count = new List<Tuple<string, int>>();
+            return count;
+        }
+
+        public static string selectLine (Array LyricList, Array selectedIndices)
+        {
+            return "this is the line";
+        }
+    }
+}
---
+++ LyrickPick/LyrickPick/Processors/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LyrickPick.Processors
+{
+    public class Song
+    {
+        static string artist;
+        static string song;
+        static string year; 
+        public Song(string artistName, string songTitle)
+        {
+            artist = artistName;
+            song = songTitle;
+        }
+    }
+}
