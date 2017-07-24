879922bcd682c4b4184e7784630068081fb4e799
jesschen <jessica_j_chen@outlook.com>
2017-07-24 15:35:33 -0700

    checking in

--- LyrickPick/LyrickPick/Processors/LyricProcessor.cs
+++ LyrickPick/LyrickPick/Processors/LyricProcessor.cs
@@ -10,31 +10,29 @@ namespace LyrickPick.Processors
     {
         //takes in json or string of one song's lyrics
         //returns array of strings of each line
-        public static string[] SpliceSong (string LyricsJson)
+        public static List<String> SpliceSong (string LyricsJson)
         {
             //array list of every line as a separate item
             var obj = JObject.Parse(LyricsJson);
             var lyricsBody = (string)obj["body"]["lyrics"]["lyrics_body"];
-            string[] lines = lyricsBody.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            List<String> lines = lyricsBody.Split('\n').ToList();
             return lines;
 
         }
-
-
         //takes in song lyrics broken into array of strings of each line
         //returns list of lines and count of each
-        public static List<Tuple<string, int>> CountLines (string[] SongLyrics)
+        public static List<Tuple<string, int>> CountLines (List<String> SongLyrics)
         {
             List<Tuple<string, int>> count = new List<Tuple<string, int>>();
             return count;
         }
 
-        public static string selectLine (string[] lines, string[] selectedLines)
+        public static string selectLine (List<String> lines, List<String> selectedLines)
         {
             string line = selectedLines[0];
             while (selectedLines.Contains(line))
             {
-                line = lines[new Random().Next(0,lines.Length)];
+                line = lines[new Random().Next(0,lines.Count)];
             }
             return line;
         }
--- LyrickPick/UnitTestProject1/ProcessorUnitTests.cs
+++ LyrickPick/UnitTestProject1/ProcessorUnitTests.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestProject1
+namespace LyrickPickUnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest
     {
         [TestMethod]
         public void TestMethod1()
