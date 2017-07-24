dd8c260447bdac5f91ebe83950e92236b0b1f53b
jigupt <jiteshgupta1192@gmail.com>
2017-07-24 13:58:05 -0700

    Creating a new class to work on

---
+++ LyrickPick/LyrickPick/Processors/FetchSongs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LyrickPick.Processors
+{
+    public class FetchSongs
+    {
+        private List<String> songsList;
+    }
+}
