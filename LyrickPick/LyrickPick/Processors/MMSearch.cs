0e35c6f4f809cbbcb878653d8b439cb4e2b47d96
jesschen <jessica_j_chen@outlook.com>
2017-07-27 18:55:53 -0700

    match genre (by dictionary, not by search)

--- LyrickPick/LyrickPick/Processors/MMSearch.cs
+++ LyrickPick/LyrickPick/Processors/MMSearch.cs
@@ -8,6 +8,35 @@ namespace LyrickPick.Processors
     {
         private static string musixmatchAPIkey = "8b7654870c8395335a30eb19039218f6";
         private string songsData = string.Empty;
+        private Dictionary<string, int> genreDict = new Dictionary<string, int>() {
+            {"blues", 2},
+            {"comedy", 3},
+            {"children's", 4},
+            {"classical", 5},
+            {"country", 6},
+            {"electronic", 7},
+            {"holiday", 8},
+            {"opera", 9},
+            {"singer", 10},
+            {"songwriter", 10},
+            {"jazz", 11},
+            {"latin", 12},
+            {"new age", 13},
+            {"pop", 14},
+            {"r&b", 15},
+            {"soul", 15},
+            {"soundtrack", 16},
+            {"dance", 17},
+            {"hip hop", 18},
+            {"rap", 18},
+            {"world", 19},
+            {"alternative", 20},
+            {"rock", 21},
+            {"christian", 22},
+            {"gospel", 22},
+            {"vocal", 23},
+            {"reggae", 24},
+            {"heavy metal", 1153} };
 
         /*
         public int matchArtist(string artist)
@@ -36,13 +65,12 @@ namespace LyrickPick.Processors
             return DataParser.GetTrackMatches(data);
         }
 
-        //return music_genre_id
-        public List<int> matchGenre(string genre)
+        //return music_genre_id (-1 if not listed)
+        public int matchGenre(string genre)
         {
-            string url = "http://api.musixmatch.com/ws/1.1/track.search?page=1&page_size=100&q_track=" + Uri.EscapeDataString(genre) + "&apikey=" + musixmatchAPIkey;
-            string data = FetchData.FetchDataFromURL(url);
-
-            return DataParser.GetTrackMatches(data);
+            if (genreDict.ContainsKey(genre))
+                return genreDict[genre];
+            return -1;
         }
 
         public int matchGenrebyArtist(string artist)
